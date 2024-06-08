89451b4c43a553f8da371cd184da1d976d8d7c81
Parth Nanda <parthnanda002@gmail.com>
2024-06-08 20:34:08 +0530

    added db

--- bitespeed/settings.py
+++ bitespeed/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -82,6 +82,7 @@ DATABASES = {
     }
 }
 
+DATABASES['default'] = dj_database_url.parse("postgres://bitespeeddb_iuja_user:k8OQplOJTveloO0JmD1sjSHEy3zn6lVY@dpg-cpi73amct0pc73fnh46g-a.oregon-postgres.render.com/bitespeeddb_iuja")
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
